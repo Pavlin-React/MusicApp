6e54c1232632149088c40519cb7c7ac361d6964f
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-09-02 15:12:46 +0300

    swiper

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, StatusBar } from 'react-native'
 import MusicPlayer from './component/MusicPlayer'
 
 
 const App = () => {
   return (
-    <MusicPlayer />
+    <View style={styles.container} >
+      <StatusBar barStyle='light-content'/>
+      <MusicPlayer />
+    </View>
   )
 }
 
 export default App
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
--- component/MusicPlayer.js
+++ component/MusicPlayer.js
@@ -1,21 +1,38 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Dimensions, StyleSheet, SafeAreaView} from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+let {width, height} = Dimensions.get('window')
 
 const MusicPlayer = () => {
  return (
-  <View style={styles.container}>
-   <Text>This is the music player</Text>
+  <SafeAreaView style={styles.container}>
+   <View style={styles.mainContainer}>
   </View>
+    <View style={{
+     borderTopColor: '#393e46',
+     borderTopWidth: 1,
+     width: width,
+     alignSelf: 'center',
+     paddingVertical: 15,
+     }} >
+     <Ionicons name='heart-outline' size={30} color='#777777'/> 
+    </View>
+  </SafeAreaView>
  )
 }
 
 export default MusicPlayer
 
 let styles = StyleSheet.create({
- container: {
+ mainContainer: {
   flex: 1,
   justifyContent: 'center',
   alignItems: 'center'
+ },
+ container: {
+  flex: 1,
+  backgroundColor: '#222831'
  }
 })
 
